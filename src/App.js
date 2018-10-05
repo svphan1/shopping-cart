f587cc6f1c6839f74ee38c538b747f12a263ddd0
svphan29 <36240410+svphan29@users.noreply.github.com>
2018-10-04 18:22:57 -0600

    on dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CartHeader from './Components/Header';
+import CartFooter from './Components/Footer';
+import CartItems from './Components/CartItems';
+import AddItem from './Components/AddItem';
 
 class App extends Component {
+
+  copyright = '2018';
+
+  state = {
+    cartItemsList: [
+      { id: 1, product: { id: 40, name: 'Millennium Falcon', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'X-Wing Starfighter', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Imperial Star Destroyer', priceInCents: 1999 }, quantity: 1 },
+    ],
+  
+    products: [
+      { id: 40, name: 'Millennium Falcon', priceInCents: 399 },
+      { id: 41, name: 'X-Wing Starfighter', priceInCents: 499 },
+      { id: 42, name: 'Imperial Star Destroyer', priceInCents: 1999 },
+      { id: 43, name: 'Naboo Royal Starship', priceInCents: 2500 },
+      { id: 44, name: 'TIE Fighter', priceInCents: 1000 },
+      { id: 45, name: 'AT-AT Walkers', priceInCents: 399 },
+      { id: 46, name: 'Death Star', priceInCents: 2999 },
+      { id: 47, name: 'Speeder Bike', priceInCents: 40000 },
+      { id: 48, name: 'Super Star Destroyer', priceInCents: 3990 },
+    ]
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <CartHeader />
         </header>
+        <p>
+          <CartItems cartItemsList={this.state.cartItemsList} />
+        </p>
+        <p>
+          <AddItem products={this.state.products} change={this.handleChange}/>
+        </p>
+        <div>
+          
+        </div>
+        <div className="container">
+        <button className="btn btn-primary" type="button">Submit</button>
+        </div>
+        <footer>
+          <CartFooter copyright={this.copyright} />
+        </footer>
       </div>
     );
   }
---
+++ src/Components/CartItems.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import CartItemComponent from './CartItem';
+import './Component.css'
+// import AddItem from './AddItem';
+
+
+const CartItems = (props) => {
+	return (
+		<div className="container">
+			<h1>Cart Items</h1>
+			<div className="list-group">
+				<div className="list-group-item">
+					<div className="row">
+						<div className="col-md-8">Product</div>
+						<div className="col-md-2">Price</div>
+						<div className="col-md-2">Quantity</div>
+					</div>
+				</div>
+				<CartItemComponent cartItemsList={props.cartItemsList} />
+			</div>
+			<label>Quantity</label>
+			<div>
+				<input className="input"></input>
+			</div>
+			<label>Products</label>
+		</div>
+	)
+};
+
+export default CartItems;
